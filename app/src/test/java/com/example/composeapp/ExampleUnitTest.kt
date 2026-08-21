09f28aef72d27a4180eba708940bce65cf9a3ac2
surafel5509-del <surafel5509@gmail.com>
2026-08-21 20:20:11 +0000

    Update ExampleUnitTest.kt via DroidStudio IDE

---
+++ app/src/test/java/com/example/composeapp/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.composeapp
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun project_configuration_isValid() {
+        val appName = "ComposeSampleApp"
+        assertTrue(appName.isNotBlank())
+    }
+}
