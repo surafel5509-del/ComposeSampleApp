c9c7be4d13f2a274248623cd6aa8bf707b049cb7
surafel5509-del <surafel5509@gmail.com>
2026-09-12 16:01:03 +0000

    fix: use Kotlin serialization plugin alias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
-    kotlin("plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
