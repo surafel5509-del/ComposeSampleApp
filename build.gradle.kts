4312170d841c2ec49cebf59868dd3ba0a45e8a68
surafel5509-del <surafel5509@gmail.com>
2026-09-12 17:09:51 +0000

    fix: migrate to AGP 9 built-in Kotlin and KSP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all sub-projects.
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("com.android.library") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("com.android.library") version "9.1.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
-    id("org.jetbrains.kotlin.kapt") version "2.2.10" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
 }
