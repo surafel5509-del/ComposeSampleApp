da952a5261b5870641556642b2c0180efda38003
surafel5509-del <surafel5509@gmail.com>
2026-09-12 16:02:12 +0000

    build: enable Kotlin annotation processing for Room

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.2.10" apply false
 }
