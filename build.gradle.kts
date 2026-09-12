31586aff2d1d8245f5fd53e625a35e84d2ef2304
surafel5509-del <surafel5509@gmail.com>
2026-09-12 16:00:56 +0000

    build: configure Kotlin serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("com.android.library") version "9.1.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
 }
